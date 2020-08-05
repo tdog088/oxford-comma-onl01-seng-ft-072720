7e639e5b2d68e7b080fe5abe9eca1643d1d7fd80
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-05 17:50:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array.first
+  
+  elsif array.length == 2 
+    return array.join(" and ")
+  
+  elsif array.length > 2 
+    array.each.collect do |item|
+      if item == array[-1]
+        array[-1] = "and " << item
+      end
+    end
+    array.join(", ")
+  end
 end
