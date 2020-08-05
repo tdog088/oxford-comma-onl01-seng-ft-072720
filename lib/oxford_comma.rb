5da336ca35f64ea6671b3cc90c85bdd91a9592a9
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-05 17:31:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(array)
   if array == 1 
     puts array.first
-  end
   
   elsif array == 2 
     puts array.join
-  end
   
   elsif array.length > 2 
     array.insert(-1, "and")
